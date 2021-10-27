90366cb964cdaeeef8398aa17ed377ee518ba2ba
pmutua <pmutua@live.com>
2021-10-27 18:02:58 +0300

    chore: Add restframework default paermission classs as DjangoModelPermissionsOrAnonReadOnly Ref: #1

--- src/src/settings.py
+++ src/src/settings.py
@@ -95,6 +95,12 @@ else:
 
     }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
